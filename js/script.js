f2a5a13d5593a5d01039067f5cc363f722b2ba6e
jaman73 <52205042+jaman73@users.noreply.github.com>
2025-01-22 01:51:46 +0600

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("contact-form");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        alert("Thank you for reaching out! I will get back to you soon.");
+    });
+});
